4ccd15f16e174b0d39ae8e751f92b1f5a87775d1
luctst <lucas.tostee@gmail.com>
2021-12-08 01:09:43 +0100

    Update dd shell script

--- bdd.sh
+++ bdd.sh
@@ -2,7 +2,7 @@
 
 getBackup() {
     echo "Retrieving last backup."
-    docker exec --env-file='./.env' -i db bash -c 'mongodump --archive="./tmp/dump-bizzy.gz" --gzip --db pswd-dev --uri "$DB_URL" && mongorestore --gzip --nsInclude="pswd-dev.*" --archive="./tmp/dump-bizzy-dev.gz"'
+    docker exec --env-file='./.env' -i db bash -c 'mongodump --archive="./tmp/<database>.gz" --gzip --db <database> --uri "$DB_URL" && mongorestore --gzip --nsInclude="<database>.*" --archive="./tmp/<database>.gz"'
 }
 
 getBackup
